6ee37139e8b06237293f09c3a2e46fdf20cb31ad
Thauan <thauantrindade@outlook.com>
2018-09-24 12:27:07 -0300

    first commit

---
+++ src/features/auth/event.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <form class="cf">
+      <h1>Cadastro de Eventos</h1>
+      <div class="half left cf">
+        <input type="text" id="input-nome" placeholder="Nome">
+        <input type="text" id="input-local" placeholder="Local">
+        <input type="text" id="input-data" placeholder="dd/mm/aaaa">
+        <input type="text" id="input-quantidade-mesas" placeholder="Quantidade de Mesas">
+      </div>
+      <input type="submit" value="Submit" id="input-submit">
+    </form>
+  </div>
+</template>
---
+++ src/features/auth/main.vue
@@ -0,0 +1,45 @@
+<script>
+
+function TestaCPF(strCPF) {
+    var Soma;
+    var Resto;
+    Soma = 0;
+  if (strCPF == "00000000000") return false;
+     
+  for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+  Resto = (Soma * 10) % 11;
+   
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
+   
+  Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+   
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+    return true;
+}
+var strCPF = "12345678909";
+alert(TestaCPF(strCPF));
+
+
+</script>
+
+<template>
+  <div>
+    <form class="cf">
+      <h1>Cadastro de Cliente</h1>
+        <div class="half left cf">
+          <input type="text" id="input-name" placeholder="Nome Completo">
+          <input type="text" id="input-cpf" placeholder="CPF">
+          <input type="text" id="input-cep" placeholder="CEP">
+          <input type="text" id="input-endereco" placeholder="Endereço">
+        </div>
+      <input type="submit" value="Submit" id="input-submit">
+    </form>
+  </div>
+</template> 
+
+
+<style src="./style.css"></style>
